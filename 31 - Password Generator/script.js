4ff0320cf484e2ae799f4a9a16c86f2f5ef13a74
Samuel Ruiz <samuelpuckett77@gmail.com>
2021-04-03 23:54:35 -0700

    add ability to generate password
    - created the js code that will be able to allow us to get an outputted generated password in the input of our html

--- 31 - Password Generator/script.js
+++ 31 - Password Generator/script.js
@@ -1,9 +1,57 @@
+const resultElement = document.getElementById("result");
+const lengthElement = document.getElementById("length");
+const uppercaseElement = document.getElementById("uppercase");
+const lowercaseElement = document.getElementById("lowercase");
+const numbersElement = document.getElementById("numbers");
+const symbolsElement = document.getElementById("symbols");
+const generateElement = document.getElementById("generate");
+const clipboardElement = document.getElementById("clipboard");
+
 const randomFunc = {
-  lower: getRandomLower,
   upper: getRandomUpper,
+  lower: getRandomLower,
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+generateElement.addEventListener("click", () => {
+  const length = Number(lengthElement.value);
+  const hasUpper = uppercaseElement.checked;
+  const hasLower = lowercaseElement.checked;
+  const hasNumber = numbersElement.checked;
+  const hasSymbol = symbolsElement.checked;
+
+  resultElement.innerText = generatePassword(
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(upper, lower, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArray.forEach((type) => {
+      const functName = Object.keys(type)[0];
+      generatedPassword += randomFunc[functName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
 
 // retruns a random lowercase letter
 function getRandomLower() {
